20dd5a62c80c555a4c4774c00e26d809c7a7f248
heltnx <helenetnx@yahoo.fr>
2023-12-08 17:17:39 +0100

    ajout commentaires

--- src/components/UserHeader/UserHeader.jsx
+++ src/components/UserHeader/UserHeader.jsx
@@ -4,14 +4,20 @@ import { useSelector } from 'react-redux';
 import Button from '../../components/button/Button';
 
 const HeaderComponent = ({ onEditNameClick }) => {
+    // Utilise useSelector pour extraire currentUserProfile du state Redux
     const currentUserProfile = useSelector((state) => state.user.currentUserProfile);
 
     return (
         <div className="header">
+
+            {/* Affiche le message de bienvenue avec le nom de l'utilisateur */}
             <h1>Welcome back<br />{currentUserProfile.userName} !</h1>
+
+            {/* Utilise un lien de React Router pour rediriger vers la page d'édition du nom */}
             <Link to="/edit-name" className='edit-button'>
                 <Button text="Edit Name" onClick={onEditNameClick} />
             </Link>
+
         </div>
     )
 };
