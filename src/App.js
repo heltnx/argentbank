f98e1f6edac441b6205ed93c00dd979af9ad1781
heltnx <helenetnx@yahoo.fr>
2023-12-16 17:33:01 +0100

    gestion formulaire edit username

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Router from './router/router';
+// REDUX
+import { useGetProfileMutation } from './services/user';
+import { useDispatch } from 'react-redux';
+import { setUser } from './features/user/userSlice';
 
-function App() {
+const App = () => {
+  const [getProfileMutation] = useGetProfileMutation();
+  // Dispatcher des actions
+  const dispatch = useDispatch();
+  const getProfile = async (token) => {
+    const profile = await getProfileMutation(`Bearer ${token}`);
+
+    // Vérification de la réponse de la mutation de profil
+    if (profile.data !== undefined && profile.data.body !== undefined) {
+      // Dispatch de l'action setUser avec le profil de l'utilisateur
+      dispatch(setUser(profile.data.body));
+    }
+  }
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      getProfile(token);
+    }
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <div className="App">
       <Router />
     </div>
   );
-}
+};
 
 export default App;
--- src/Layouts/form_edit/EditForm.jsx
+++ src/Layouts/form_edit/EditForm.jsx
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 // components
 import Button from '../../components/button/Button';
 // style
 import './edit-form.css'
 // REDUX
+import { useDispatch } from 'react-redux';
 import { useCurrentUserProfile } from '../../features/user/userProfile';
+import { useUpdateUserNameMutation } from "../../services/user.js"
+import { setUser } from '../../features/user/userSlice.js'
 
-const EditForm = () => {
-
+const EditForm = ({ onClose }) => {
+    const dispatch = useDispatch();
     const currentUserProfile = useCurrentUserProfile();
+    const [newUserName, setNewUserName] = useState(currentUserProfile.userName);
+    const [updateUserNameMutation] = useUpdateUserNameMutation();
+
+    const handleSave = async (e) => {
+
+        e.preventDefault();
+
+        try {
+            const token = `Bearer ${localStorage.getItem('token')}`;
+
+            const response = await updateUserNameMutation({ newUserName, token });
+            if (response.data) {
+
+                dispatch(setUser(response.data.body));
+                onClose();
+            }
+
+        } catch (error) {
+            console.error('Failed to update user name:', error);
+        }
+    };
 
     return (
+
         <div className="sign-in-content edit-form">
+
             <h2>Edit user info</h2>
 
             <form className='sign-in edit-form'>
@@ -21,7 +47,8 @@ const EditForm = () => {
                     <input
                         type="text"
                         id="userName"
-                        defaultValue={currentUserProfile.userName}
+                        value={newUserName}
+                        onChange={(e) => setNewUserName(e.target.value)}
                     />
                 </div>
 
@@ -45,8 +72,8 @@ const EditForm = () => {
                     />
                 </div>
                 <div className="input-wrapper button-wrapper">
-                    <Button className="sign-in-button comp-button" text="Save" />
-                    <Button className="sign-in-button comp-button" text="Cancel" />
+                    <Button className="sign-in-button comp-button" text="Save" onClick={handleSave} />
+                    <Button className="sign-in-button comp-button" text="Cancel" onClick={onClose} />
                 </div>
             </form>
 
