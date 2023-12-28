d59ce9cdf9fd5e0d2b0ce85ea52037b99c28d9d2
heltnx <helenetnx@yahoo.fr>
2023-12-28 19:28:46 +0100

    comentaires

--- src/layouts/form_sign_in/SignForm.jsx
+++ src/layouts/form_sign_in/SignForm.jsx
@@ -7,8 +7,12 @@ import { useLoginMutation, useGetProfileMutation } from '../../services/user';
 import { useUserActions } from '../../features/user/userActions';
 
 const SignForm = () => {
+
+    // Utilisation du hook useState pour gérer l'état local du composant
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+
+    // Utilisation de mutations pour effectuer des requêtes avec le service user
     const [loginMutation, { isError }] = useLoginMutation();
     const [getProfileMutation] = useGetProfileMutation();
     const navigate = useNavigate();
@@ -16,25 +20,29 @@ const SignForm = () => {
     // instance de useUserActions
     const { setUser } = useUserActions();
 
+    // Fonction de gestion de la soumission du formulaire de connexion
     const handleSignIn = async (e) => {
         e.preventDefault();
 
+        // Appel de la mutation pour effectuer la connexion
         const response = await loginMutation({
             email: email,
             password: password,
         });
 
         if (response.data?.body) {
+            // Récupération du token et stockage dans le localStorage
             const token = response.data.body.token;
-
             localStorage.setItem("token", token);
 
+            // Appel de la mutation pour récupérer le profil de l'utilisateur
             const profile = await getProfileMutation(`Bearer ${token}`);
 
             if (profile.data?.body) {
-                // "setUser" de "useUserActions" au lieu de dispatch(setUser())
+                // useUserActions pour mettre à jour l'état Redux
                 setUser(profile.data.body);
 
+                // Redirection vers la page utilisateur
                 navigate("/user");
             } else {
                 console.error("La réponse du profil est invalide.", response);
@@ -43,14 +51,16 @@ const SignForm = () => {
             console.error("La réponse de la requête est invalide.", response);
         }
 
+        // Réinitialisation des champs
         setEmail("");
         setPassword("");
     };
 
+    // Rendu du composant formulaire de connexion
     return (
         <form className='sign-in'>
             <div className="input-wrapper">
-                <label htmlFor="username">User name</label>
+                <label htmlFor="username">Nom d'utilisateur</label>
                 <input
                     type="text"
                     id="username"
@@ -61,7 +71,7 @@ const SignForm = () => {
                 />
             </div>
             <div className="input-wrapper">
-                <label htmlFor="password">Password</label>
+                <label htmlFor="password">Mot de passe</label>
                 <input
                     type="password"
                     id="password"
@@ -71,9 +81,9 @@ const SignForm = () => {
                     }}
                 />
             </div>
-            <Checkbox id="remember-me" label="Remember me" />
-            <Button className="sign-in-button comp-button" text="Sign In" onClick={handleSignIn} />
-            {isError ? <p className='error'>Identifant ou mot de passe erroné</p> : ""}
+            <Checkbox id="remember-me" label="Se souvenir de moi" />
+            <Button className="sign-in-button comp-button" text="Se Connecter" onClick={handleSignIn} />
+            {isError ? <p className='error'>Identifiant ou mot de passe erroné</p> : ""}
         </form>
     );
 };
